93b9e4d214a0867ccdf0ac075bd9bfcbb95fa730
r6cky <cleitonrocha321@hotmail.com>
2023-01-09 15:13:11 -0300

    fiX: correcting conflicts of merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { UserProvider } from "./pages/register/contextRegister/ContextRegister";
 import { RoutesMain } from "./routes/routsmain";
-=======
-import React from 'react';
-
-import { RoutesMain } from './routes/routsmain';
->>>>>>> 35821027d8222009b021a0ec4c2752edba6a12fc
 
 function App() {
   return (
