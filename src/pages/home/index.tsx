db68594d561efdb9dd860ad8dcc3a817d4117aea
r6cky <cleitonrocha321@hotmail.com>
2023-01-10 15:51:40 -0300

    feat: create options menu of page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,7 +19,23 @@ export const HomePage = () => {
 
                   <div className="image-after-header-large">
                         <img src={ImageAfterHeaderLarge} alt="" />
-                    </div> 
+                  </div> 
+
+                  <div className='menu-large'>
+                        <div className='all-donations'>
+                            <h3>Todas as doações</h3>
+                        </div>
+                        <div className='filter-menu-container'>
+                            <h4 className='filter-menu-title'>Procurando por ?</h4>
+                            <ul className='list-filter-menu'>
+                                <li>Roupas</li>
+                                <li>Brinquedos</li>
+                                <li>Eletro</li>
+                                <li>Móveis</li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </StyledFigureBanner>
                 <DashboardList/>
 
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -35,6 +35,13 @@ export const StyledFigureBanner = styled.figure`
 
     }
 
+    .menu-large{
+        display: none;
+    }
+
+
+    
+
     @media(min-width: 780px) {
 
         .image-after-header{
@@ -67,6 +74,10 @@ export const StyledFigureBanner = styled.figure`
         }
 
 
+    }
+
+    .menu-large{
+        display: none;
     }
     }
 
@@ -106,6 +117,78 @@ export const StyledFigureBanner = styled.figure`
 
 
     }
+
+    .menu-large{
+            width: 90%;
+            max-width: 100%;
+            height: 80px;
+            margin: 0 auto;
+            border:  solid var(--color-primary) 0.1px;
+            display: flex;
+            border-radius: 8px;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 30px;
+            
+            h3{
+                width: 250px;
+                height: 80px;
+                padding: 10px;
+                background: var(--information);
+                color: var(--grey-0);
+                font-weight: bold;
+                border: none;
+                border-radius: 8px 0px 0px 8px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            
+
+            .filter-menu-container{
+                display: flex;
+                align-items: center;
+                width: 800px;
+                max-width: 100%;
+                display: flex;
+                justify-content: space-between;
+                padding: 5px;
+
+                .filter-menu-title{
+                   color: var(--information);
+                   font-weight: bold;
+                   font-size: 18px;
+                }
+
+            }
+
+            .list-filter-menu{
+                width: 600px;
+                max-width: 100%;
+                display: flex;
+                justify-content: space-around;
+
+
+                li{
+                    width: 110px;
+                    height: 30px;
+                    padding: 5px;
+                    border-radius: 8px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background: var(--color-primary);
+                    color: var(--grey-0);
+                    font-weight: bold;
+                    cursor: pointer;
+                }
+            }
+
+
+        }
+
+
+
     }
 
 `
